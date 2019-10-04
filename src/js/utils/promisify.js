60954b31b42df5ce179d7e775829af89c1623814
plotnikov-ep <plotnikov-ep@yandex-team.ru>
2019-10-04 13:43:24 +0500

    fix promisify util

--- src/js/utils/promisify.js
+++ src/js/utils/promisify.js
@@ -1,10 +1,10 @@
-const promisify = (func, data) => new Promise(resolve => {func(data, resolve)});
+const promisify = (func, context, data) => new Promise(resolve => func.call(context, data, resolve));
 
-const getStorage = (data) => promisify(chrome.storage.local.get, data);
-const getSyncStorage = (data) => promisify(chrome.storage.sync.get, data);
-const setStorage = (data) => promisify(chrome.storage.local.set, data);
-const setSyncStorage = (data) => promisify(chrome.storage.sync.set, data);
-const sendMessage = (data) => promisify(chrome.runtime.sendMessage, data);
+const getStorage = (data) => promisify(chrome.storage.local.get, chrome.storage.local, data);
+const getSyncStorage = (data) => promisify(chrome.storage.sync.get, chrome.storage.sync, data);
+const setStorage = (data) => promisify(chrome.storage.local.set, chrome.storage.local, data);
+const setSyncStorage = (data) => promisify(chrome.storage.sync.set, chrome.storage.sync, data);
+const sendMessage = (data) => promisify(chrome.runtime.sendMessage, chrome.runtime, data);
 
 const sleep = (time) =>  new Promise(resolve => {setTimeout(resolve, time)});
 
